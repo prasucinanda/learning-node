7841589a402aff9e77f0f3703996996d34d8320b
prasuci27 <prasucinanda511@gmail.com>
2024-05-12 20:55:13 +0700

    get genre movie

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const sum = require("./sum");
+const Joi = require("joi");
+const express = require("express");
+const app = express();
 
-console.log("Hello world!!");
-console.log("Hello world lagi ");
+//app.request(express.json());
 
-console.log(sum(1, 2));
+const genres = [
+  { id: 1, genre: "horor" },
+  { id: 2, genre: "comedy" },
+  { id: 3, genre: "thriller" },
+  { id: 4, genre: "romance" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Movie Land");
+});
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
