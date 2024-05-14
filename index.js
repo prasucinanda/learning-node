f77c703a9d2a0dc2497f434fe70c98c1be57b416
prasuci27 <prasucinanda511@gmail.com>
2024-05-14 22:06:23 +0700

    add validate movie

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 app.request(express.json());
 
 const genres = [
-  { id: 1, genre: "horor" },
-  { id: 2, genre: "comedy" },
-  { id: 3, genre: "thriller" },
-  { id: 4, genre: "romance" },
+  { id: 1, name: "horor" },
+  { id: 2, name: "comedy" },
+  { id: 3, name: "thriller" },
+  { id: 4, name: "romance" },
 ];
 
 app.get("/", (req, res) => {
@@ -18,6 +18,25 @@ app.get("/", (req, res) => {
 app.get("/api/genres", (req, res) => {
   res.send(genres);
 });
+
+app.post("/api/genres", (req, res) => {
+  const { error } = validateMovie(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+function validateMovie(genre) {
+  const schema = {
+    name: Joi.string().min(5).required(),
+  };
+  return Joi.validate(genre.schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port} ...`));
